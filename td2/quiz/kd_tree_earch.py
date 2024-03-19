59e3d1e95e1f7b53a60aa10fb03c1570d38222d4
Abhim03 <sadeghabderahim@gmail.com>
2024-03-19 23:00:43 +0100

    .

--- td2/quiz/kd_tree_earch.py
+++ td2/quiz/kd_tree_earch.py
@@ -9,12 +9,18 @@ print('\nbuilding kd-tree...', end='')
 kdt = KDTree(data.iloc[:,0:4], leaf_size=1, metric='euclidean')
 print(' done')
 
-x = [1.1, 2.2, 3.3, 4.4]
+x = [41.1, 32.2, 23.3, 14.4]
 print('\n  my measurements [sepal_len, sepal_wid, petal_len, petal_wid] are: ', x)
 
-dist, idx = kdt.query([x], k=1)
-print(f'    -> the iris {x} is closest to type {data.iloc[idx[0, 0],4]}')
 
-print(f'\t Index/indices of the {len(idx[0])} closest point(s): {idx[0]}')
-print(f'\t Distance(s) to the {len(dist[0])} closest point(s): {dist[0]}')
+# Modify the kdt.query call to search for the 5 nearest neighbors
+distances, indices = kdt.query([x], k=5)
+
+# Calculate the average distance
+average_distance = distances[0].mean()
+
+print(f'\t Index/indices of the {len(indices[0])} closest point(s): {indices[0]}')
+print(f'\t Distance(s) to the {len(distances[0])} closest point(s): {distances[0]}')
+print(f'\t Average distance to the {len(distances[0])} closest point(s): {average_distance}')
+
 
